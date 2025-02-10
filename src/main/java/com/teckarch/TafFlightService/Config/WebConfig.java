abf7a39ca58804c08c8f5d7c87a3448fbb9da030
Akash V <akashv@tekarch.com>
2025-02-10 11:09:05 -0500

    My first code push

---
+++ src/main/java/com/teckarch/TafFlightService/Config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.teckarch.TafFlightService.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Allow all endpoints
+                        .allowedOriginPatterns("*") // Allow any IP or domain
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+                        .allowedHeaders("*") // Allow all headers
+                        .allowCredentials(true); // Allow cookies/auth headers
+            }
+        };
+    }
+}
